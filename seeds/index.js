f89e5b00b492dc95ae5c67a1ae75b772fa748794
PraisedL <lucassachet95@hotmail.com>
2021-07-24 12:18:48 -0300

    commit

--- seeds/index.js
+++ seeds/index.js
@@ -20,18 +20,21 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '60eafed602556e18101ddc80',
+            author: '60ee038aa56ab3168804bff3',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis enim corporis nemo iusto inventore laboriosam libero aliquam rerum suscipit ipsa magnam nihil autem, voluptates accusantium? Dolore ducimus minus facilis illum.',
             price,
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
             },
             images: [
                 {
